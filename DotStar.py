a46316486315d434ff45203ceb081d33d3e49a5d
RobertKatz <ktzrob006@myuct.ac.za>
2017-10-30 11:09:34 +0200

    asdf

--- DotStar.py
+++ DotStar.py
@@ -1,3 +1,11 @@
+import time
+from dotstar import Adafruit_DotStar
+datapin   = 23
+clockpin  = 24
+strip     = Adafruit_DotStar(numpixels, datapin, clockpin)
+strip.begin()           # Initialize pins for output
+strip.setBrightness(64) # Limit brightness to ~1/4 duty cycle
+
 count = 0;
 while True:
 	counter = 0;
@@ -7,9 +15,11 @@ while True:
 				s=f.readline();
 				ss = s.split(',');
 
-				print(ss[0],ss[1],ss[2])
-
+				color = ss[0]<<8+ss[1]<<8+ss[2]
+				strip.setPixelColor(i, color)
 				counter=counter+1
+				strip.show()                     # Refresh strip
+       			time.sleep(1.0 / 50)
 
 		count+=1
 	else:
