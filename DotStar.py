717dd2e3b6476a4537eab822069f8d8ffd44486a
RobertKatz <ktzrob006@myuct.ac.za>
2017-10-30 11:56:22 +0200

    asdf

--- DotStar.py
+++ DotStar.py
@@ -1,34 +1,32 @@
-# import time
-import numpy as np
-# from dotstar import Adafruit_DotStar
-# numpixels = 592 # Number of LEDs in strip
+import time
+from dotstar import Adafruit_DotStar
 
-# datapin   = 23
-# clockpin  = 24
-# strip     = Adafruit_DotStar(numpixels, datapin, clockpin)
-# strip.begin()           # Initialize pins for output
-# strip.setBrightness(64) # Limit brightness to ~1/4 duty cycle
+numpixels = 592
+datapin   = 23
+clockpin  = 24
+strip     = Adafruit_DotStar(numpixels)
+strip.begin()           # Initialize pins for output
+strip.setBrightness(64) # Limit brightness to ~1/4 duty cycle
+
+x []
+for i in range(400):
+	with open("./textfiles/"+str(i)+'.txt') as f:
+		x[i].append(int(f.readLine()));
+
+
 
 count = 0;
 while True:
-	counter = 0;
-	if(count < 400):
-		with open("./textfiles/"+str(count)+'.txt') as f:
-			for i in range(592):
-				s=f.readline();
-				ss = s.split(',');
-
-                color = (np.uint8(ss[0])<<8)+(np.uint8(ss[1])<<8)+(np.uint8(ss[2])<<8)
-                counter=counter+1
-                print(color)
-
-				#strip.setPixelColor(i, color)
-				#strip.show()                     # Refresh strip
-       			#time.sleep(1.0 / 50)
-
-		count+=1
-	else:
-		count=0
-		break
-
-
+        counter = 0;
+        if(count < 400):
+               
+		for i in range(592):
+			color = x[count][i]
+			strip.setPixelColor(i, color)
+			counter=counter+1
+			strip.show()                     # Refresh strip
+			time.sleep(1.0 / 50)
+            print(count)
+            count+=1
+        else:
+                count=0
