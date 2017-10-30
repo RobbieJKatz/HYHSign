e09850967565c86cc34a7cce390f4e0b81f2087b
RobertKatz <ktzrob006@myuct.ac.za>
2017-10-30 10:58:27 +0200

    asdf

--- DotStar.py
+++ DotStar.py
@@ -1,13 +1,13 @@
 count = 0;
 while True:
 	counter = 0;
-	if(count < 400):
+	if(count < 1):
 		with open("./textfiles/"+str(count)+'.txt') as f:
 			for i in range(592):
 				s=f.readline();
 				ss = s.split(',');
-				y=l[counter][0]
-				x=l[counter][1]
+
+				print(ss[0],ss[1],ss[2])
 
 				counter=counter+1
 
